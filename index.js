5bfad54876ab640fc9f1537bb1b63ec126c4135e
tasnimabedeen <99178083+tasnimabedeen@users.noreply.github.com>
2023-11-26 21:05:05 +0600

    real time window position
    This commit includes
    -Updates window in real time
    -bug fixes related to mouse position
    -bug fixes related window resize
    -renamed variables into more recognizable ones.

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const bar = document.getElementById('bar');
 let pos0 = 0;
 var totalwidth = window.innerWidth;
-let posX = totalwidth - 100;
-
+window.addEventListener("resize", () => {
+    totalwidth = window.innerWidth;
+});
 window.addEventListener('mousemove', (event) => {
-    const pos1 = event.pageX;
-    let pos2 = 50;
-    let pos3 = pos1 - pos2;
-    if (pos1 < 50) {
+    const mousepos = event.pageX;
+    let offset = 50;
+    let barpos = mousepos - offset;
+    if (mousepos < 50) {
         bar.style.left = '0px';
     }
-    else if(Math.abs(totalwidth - pos1) < 50) {
-        bar.style.left = posX + 'px';
+    else if (Math.abs(totalwidth - mousepos) < 50) {
+        bar.style.left = totalwidth - 100 + 'px';
     }
     else {
-        bar.style.left = pos3 + 'px';
+        bar.style.left = barpos + 'px';
     }
 });
 
