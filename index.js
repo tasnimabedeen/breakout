7c1b8d8c172bebde225a85454395bae9368778ef
tasnimabedeen <99178083+tasnimabedeen@users.noreply.github.com>
2023-11-28 00:01:09 +0600

    updated animation

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const bar = document.getElementById('bar');
 let pos0 = 0;
 var totalwidth = window.innerWidth;
+var totalheight = window.innerHeight;
+
 window.addEventListener("resize", () => {
     totalwidth = window.innerWidth;
+    totalheight = window.innerHeight;
 });
 window.addEventListener('mousemove', (event) => {
     const mousepos = event.pageX;
@@ -21,61 +24,70 @@ window.addEventListener('mousemove', (event) => {
 
 
 
-let id = null;
 const elem = document.getElementById("ball");
 let pos1 = 300;
 let pos2 = 0;
-clearInterval(id);
-id = setInterval(frame, 10);
-function frame() {
-    if (pos1 == 0) {
-        clearInterval(id);
-        let idtop = null;
-        const elem2 = document.getElementById("ball");
-        let pos3 = pos1;
-        let pos4 = totalwidth - pos2 - 15;
-        clearInterval(idtop);
-        idtop = setInterval(frame2, 10);
-        function frame2() {
-            if (pos4 == 0) {
-                clearInterval(id);
 
+for (let n = 0; n < 1; n++) {
+    let id = null;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos1 == 0) {
+            clearInterval(id);
+            let idtop = null;
+            let pos3 = totalwidth - pos2 - 15;
+            clearInterval(idtop);
+            idtop = setInterval(frame2, 10);
+            function frame2() {
+                if (pos3 == 0) {
+                    clearInterval(idtop);
+                    let idright = null;
+                    let pos4 = totalheight - 40 - pos3;
+                    clearInterval(idright);
+                    idright = setInterval(frame3, 10);
+                    function frame3() {
+                        if (pos4 == 0) {
+                            clearInterval(idright);
+                            let idbottom = null;
+                            clearInterval(idbottom);
+                            idbottom = setInterval(frame4, 10);
+                            function frame4() {
+                                if (pos2 == 0) {
+                                    clearInterval(idbottom);
+                                }
+                                else {
+                                    pos1--;
+                                    pos2--;
+                                    elem.style.top = pos1 + "px";
+                                    elem.style.left = pos2 + "px";
+                                }
+                            }
+                        }
+                        else {
+                            pos1++;
+                            pos2--;
+                            elem.style.top = pos1 + "px";
+                            elem.style.left = pos2 + "px";
+                            pos4 = window.innerHeight - 55 - pos1;
+                        }
+                    }
+                }
+                else {
+                    pos1++;
+                    pos2++;
+                    elem.style.top = pos1 + "px";
+                    elem.style.left = pos2 + "px";
+                    pos3 = totalwidth - pos2 - 15;
+                }
             }
-            else if (pos3 => 0) {
-                pos3++;
-                pos2++;
-                elem2.style.top = pos3 + "px";
-                elem2.style.left = pos2 + "px";
-                pos4 = totalwidth - pos2 - 15;
-            }
+        } 
+        else {
+            pos1--;
+            pos2++;
+            elem.style.top = pos1 + "px";
+            elem.style.left = pos2 + "px";
         }
-    } else if (pos2 => 0) {
-        pos1--;
-        pos2++;
-        elem.style.top = pos1 + "px";
-        elem.style.left = pos2 + "px";
-
-    }
-
-
-}
-/*
-let idbottom = null;
-const elem3 = document.getElementById("ball");
-let pos5 = window.innerHeight - 40 - pos3;
-let pos6 = pos2;
-clearInterval(idbottom);
-idbottom = setInterval(frame3, 10);
-function frame3() {
-    if (pos5 == 0) {
-        clearInterval(idbottom);
-    }
-    else {
-        pos3++;
-        pos6--;
-        elem3.style.top = pos3 + "px";
-        elem3.style.left = pos6 + "px";
-        pos5 = window.innerHeight - 40 - pos3;
     }
 }
-*/
+
